f428f977c504ddf52faca8ae4d1c9dc69ab300f0
Kiddi <kristinnv12@ru.is>
2015-02-26 23:28:15 +0000

    early handin 1

--- mm.c
+++ mm.c
@@ -49,6 +49,12 @@ team_t team = {
     ""
 };
 
+/* Basic constants and macros (from mm-firstfit.c)*/
+#define REQSIZE       8       /* doubleword size (bytes) */
+#define OVERHEAD    8       /* overhead of header and footer (bytes) */
+#define WSIZE       4       /* word size (bytes) */
+#define CHUNKSIZE  (1<<12)  /* initial heap size (bytes) */
+
 /* single word (4) or double word (8) alignment */
 #define ALIGNMENT 8
 
@@ -58,11 +64,38 @@ team_t team = {
 
 #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
 
+#define MAX(x, y) ((x) > (y)? (x) : (y))  
+
+/* Pack a size and allocated bit into a word */
+#define PACK(size, alloc)  ((size) | (alloc))
+
+/* Read and write a word at address p */
+#define GET(p)       (*(size_t *)(p))
+#define PUT(p, val)  (*(size_t *)(p) = (val))  
+
+/*Read the size and allocated fields from address p */
+#define GET_SIZE(p)  (GET(p) & ~0x7)
+#define GET_ALLOC(p) (GET(p) & 0x1)
+
+/* Given block ptr bp, compute address of its header and footer */
+#define HDRP(bp)       ((char *)(bp) - WSIZE)  
+#define FTRP(bp)       ((char *)(bp) + GET_SIZE(HDRP(bp)) - REQSIZE)
+
+/* Given block ptr bp, compute address of next and previous blocks */
+#define NEXT_BLKP(bp)  ((char *)(bp) + GET_SIZE(((char *)(bp) - WSIZE)))
+#define PREV_BLKP(bp)  ((char *)(bp) - GET_SIZE(((char *)(bp) - REQSIZE)))
+
+static char *heap_start;  /* pointer to first block */  
+
+static void *scan_for_free(size_t adjsize);
+
 /* 
  * mm_init - initialize the malloc package.
  */
 int mm_init(void)
 {
+    //TODO: Find the Start of the heap and store it globaly.
+    heap_start = 0;
     return 0;
 }
 
@@ -72,21 +105,59 @@ int mm_init(void)
  */
 void *mm_malloc(size_t size)
 {
-    int newsize = ALIGN(size + SIZE_T_SIZE);
-    void *p = mem_sbrk(newsize);
-    if (p == (void *)-1)
-	return NULL;
-    else {
-        *(size_t *)p = size;
-        return (void *)((char *)p + SIZE_T_SIZE);
+
+    size_t adjsize;
+    char *alocspacePtr;
+
+    //base case: 0
+    if(size <=0)
+    {
+        return NULL;
+    }
+
+    //Must make our size a modulo 0 + overhead of 8 bytes
+    if (size <= REQSIZE)
+    adjsize = REQSIZE + OVERHEAD;
+    else
+    adjsize = REQSIZE * ((size + (OVERHEAD) + (REQSIZE-1)) / REQSIZE);
+    
+    //scan for free space
+    alocspacePtr = scan_for_free(adjsize);
+
+    if(alocspacePtr != NULL){
+
+        //found free space that fits the adjusted size
+        //TODO: understand place function and impliment
+        return alocspacePtr;
+    }
+    else
+    {
+        //we just allocate more space
+        //TODO: Find a more optimal way of finding how much space we need to reserve
+        int newsize = ALIGN(size + SIZE_T_SIZE);
+        void *p = mem_sbrk(newsize);
+        if (p == (void *)-1)
+    	return NULL;
+        else {
+            *(size_t *)p = size;
+            return (void *)((char *)p + SIZE_T_SIZE);
+        }
     }
 }
 
 /*
  * mm_free - Freeing a block does nothing.
  */
-void mm_free(void *ptr)
+void mm_free(void *williamWallace)
 {
+    size_t ptrSize = GET_SIZE(HDRP(williamWallace));
+
+    //Free the header and footer of given pointer
+    PUT(HDRP(williamWallace), PACK(ptrSize, 0));
+    PUT(FTRP(williamWallace), PACK(ptrSize, 0));
+    //FREEDOM!!!
+
+    //TODO: need to check if the freed space can be joined with the next or previous block
 }
 
 /*
@@ -94,6 +165,7 @@ void mm_free(void *ptr)
  */
 void *mm_realloc(void *ptr, size_t size)
 {
+    //TODO: Check if it is possible to extend the current memory adress rather than just reserving more space
     void *oldptr = ptr;
     void *newptr;
     size_t copySize;
@@ -107,4 +179,21 @@ void *mm_realloc(void *ptr, size_t size)
     memcpy(newptr, oldptr, copySize);
     mm_free(oldptr);
     return newptr;
+}
+
+/*
+* scan_for_free - Scans the heap for the requierd size.
+*/
+static void *scan_for_free(size_t reqsize)
+{
+    void *curr;
+
+    //Start on the head of the heap and run down it
+    for (curr = heap_start; GET_SIZE(HDRP(curr)) > 0; curr = NEXT_BLKP(curr)) {
+        //Found space fits the requierd size
+        if (!GET_ALLOC(HDRP(curr)) && (reqsize <= GET_SIZE(HDRP(curr)))) {
+            return curr;
+        }
+    }
+    return NULL; /* need more space */
 }
