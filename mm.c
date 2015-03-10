6a93025eb2cd2b13aa96713c8f4d9edc634dec28
ragnarp12 <ragnarp12@ru.is>
2015-03-10 09:17:08 +0000

    Be happy

--- mm.c
+++ mm.c
@@ -1,6 +1,6 @@
 /*
  * mm.c - Explicit free list solution using LIFO free policy, optimal fit placement (perfect fit search) and boundary tag coalescing.
- * 
+ *
  * Our solution will be a explicit free list. We will free blocks using the LIFO policy for simplicity, that is a newly freed block will be
  * added at the begining/root of our free list. For our list we will need 2 extra word per each block for our links, that is the forward
  * and back pointer since our next free block could be anywhere in the physical memory, that is the list does not lie linear in memory
@@ -11,19 +11,19 @@
  * if the perfect fit does not exist we will try again but lower our expectations and make do with a little bit of
  * wasted space, we will try to find the perfect balance in time usage and the amount of fragmentation we are
  * willing to put up with until we get desprate and expand our heap space by incrementing our brk pointer.
- * We are going to store a pointer to the start of our heap in a global varialble and this variable will 
- * be reset at initilazation. 
+ * We are going to store a pointer to the start of our heap in a global varialble and this variable will
+ * be reset at initilazation.
  *
  *  This is how each free block should be structured:
  *
  *      |--------------------------------------------------------------------------------|
  *      | Size boundary tag| Nexr ptr | Prev ptr | Payload & padding | Size bounadry tag |
  *      |--------------------------------------------------------------------------------|
- *  
+ *
  *  And this is how the list it self should be structured (pretty much the same idea as in the implicit list solution):
  *
  *      |-------------------------------------------------------------------------|
- *      | Prolog block (header & footer) | The free blocks | Epilog block (header)| 
+ *      | Prolog block (header & footer) | The free blocks | Epilog block (header)|
  *      |-------------------------------------------------------------------------|
  */
 #include <stdio.h>
@@ -37,11 +37,11 @@
 
 /*********************************************************
  * === User information ===
- * Group: PlainStupid 
+ * Group: PlainStupid
  * User 1: kristinnv12
  * SSN: 0208923829
- * User 2: ragnarp12 
- * SSN: 2801872169 
+ * User 2: ragnarp12
+ * SSN: 2801872169
  * === End User Information ===
  ********************************************************/
 
@@ -65,7 +65,7 @@ team_t team = {
 /* Basic constants and macros (from mm-firstfit.c)*/
 
 #define REQSIZE     8       /* doubleword size (bytes) */
-#define DSIZE     8 
+#define DSIZE     8
 //TODO change over head to include next and prev pointers
 #define OVERHEAD    8       /* overhead of header and footer (bytes) */
 #define WSIZE       4       /* word size (bytes) */
@@ -80,57 +80,57 @@ team_t team = {
 
 #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
 
-#define MAX(x, y) ((x) > (y)? (x) : (y))  
+#define MAX(x, y) ((x) > (y)? (x) : (y))
 
 /* Pack a size and allocated bit into a word */
 #define PACK(size, alloc)  ((size) | (alloc))
 
 /* Read and write a word at address p */
 #define GET(p)       (*(size_t *)(p))
-#define PUT(p, val)  (*(size_t *)(p) = (val))  
+#define PUT(p, val)  (*(size_t *)(p) = (val))
 
 /*Read the size and allocated fields from address p */
 #define GET_SIZE(p)  (GET(p) & ~0x7)
 #define GET_ALLOC(p) (GET(p) & 0x1)
 
 /* Given block ptr bp, compute address of its header and footer */
-#define HDRP(bp)       ((char *)(bp) - WSIZE)  
+#define HDRP(bp)       ((char *)(bp) - WSIZE)
 #define FTRP(bp)       ((char *)(bp) + GET_SIZE(HDRP(bp)) - REQSIZE)
 
 /* Given block ptr bp, compute address of next and previous blocks */
 #define NEXT_BLKP(bp)  ((char *)(bp) + GET_SIZE(((char *)(bp) - WSIZE)))
 #define PREV_BLKP(bp)  ((char *)(bp) - GET_SIZE(((char *)(bp) - REQSIZE)))
 
-static char *heap_start;  /* pointer to the start of out heap*/ 
+static char *heap_start;  /* pointer to the start of out heap*/
 
 static void *scan_for_free(size_t adjsize);
 static void *new_free_block(size_t words);
 static void place(void *alloc_ptr, size_t size_needed);
 static void *coalesce(void *middle);
 
-/* 
+/*
  * mm_init - should find the start of the heap and reserve some initial space.
  */
 int mm_init(void)
-{   
-    heap_start = mem_sbrk(4*WSIZE); //increment the break pointer by two double words
+{
+    heap_start = mem_sbrk(4 * WSIZE); //increment the break pointer by two double words
 
-    if(heap_start == NULL)
+    if (heap_start == NULL)
     {
         return -1; //No more space for heap;
     }
-                                                        //                                  -----------
+    //                                  -----------
     PUT(heap_start, 0);                                 //padding                           | padding |
-                                                        //                                  |---------|
+    //                                  |---------|
     PUT(heap_start + WSIZE, PACK(OVERHEAD, 1));         //prolog header                     |   PH    |
-                                                        //                                  |---------|
+    //                                  |---------|
     PUT(heap_start + REQSIZE, PACK(OVERHEAD, 1));       //prolog footer                     |   PF    |
-                                                        //                                  |---------|
+    //                                  |---------|
     PUT(heap_start + REQSIZE + WSIZE, PACK(0, 1));      //epilog header                     |   EH    |
-                                                        //                                  -----------
+    //                                  -----------
     heap_start += REQSIZE;
 
-    if(new_free_block(CHUNKSIZE/WSIZE) == NULL )    //initilize some starting free space
+    if (new_free_block(CHUNKSIZE / WSIZE) == NULL ) //initilize some starting free space
     {
         return -1;
     }
@@ -145,22 +145,22 @@ int mm_init(void)
  */
 static void *new_free_block(size_t words)
 {
-    
+
     char *mr_clean;   //pointer to the new free/clean block
     size_t bytes;     //the number of bytes needed for the amount of words
 
-    if(words % 2)     //need to keep alignment as an even number
+    if (words % 2)    //need to keep alignment as an even number
     {
         bytes = (words + 1) * WSIZE;
     }
     else
     {
-	   bytes = words * WSIZE;
+        bytes = words * WSIZE;
     }
 
     mr_clean = mem_sbrk(bytes);  //increment the brk pointer to get more space
 
-    if(mr_clean == (void *)-1)
+    if (mr_clean == (void *) - 1)
     {
         return NULL;    //something went terribly wrong
     }
@@ -177,7 +177,7 @@ static void *new_free_block(size_t words)
     return coalesce(mr_clean);
 
 }
-/* 
+/*
  * mm_malloc - find a free block that fits our size so that it is a modulo 0 + overhead of 8 bytes
  *             if no space is found we increment mem_sbrk pointer for our new memory
  */
@@ -188,7 +188,7 @@ void *mm_malloc(size_t size)
     size_t extend_size;
 
     //base case: 0
-    if(size <= 0)
+    if (size <= 0)
     {
         return NULL;
     }
@@ -200,13 +200,13 @@ void *mm_malloc(size_t size)
     }
     else
     {
-        adjsize = REQSIZE * ((size + (OVERHEAD) + (REQSIZE-1)) / REQSIZE);
+        adjsize = REQSIZE * ((size + (OVERHEAD) + (REQSIZE - 1)) / REQSIZE);
     }
-    
+
     //scan for free space
     allocspacePtr = scan_for_free(adjsize);
 
-    if(allocspacePtr != NULL){
+    if (allocspacePtr != NULL) {
 
         //found free space that fits the adjusted size
         place(allocspacePtr, adjsize);
@@ -216,9 +216,9 @@ void *mm_malloc(size_t size)
     extend_size = MAX(adjsize, CHUNKSIZE);
     //we just allocate more space
 
-    allocspacePtr = new_free_block(extend_size/WSIZE);
+    allocspacePtr = new_free_block(extend_size / WSIZE);
 
-    if(allocspacePtr == NULL)
+    if (allocspacePtr == NULL)
     {
         return NULL;
     }
@@ -232,12 +232,12 @@ void *mm_malloc(size_t size)
  */
 static void place(void *alloc_ptr, size_t size_needed)
 {
-    
+
     size_t block_size = GET_SIZE(HDRP(alloc_ptr));      //fetch the size of the block given to us
 
     size_t block_remainder = block_size - size_needed;
 
-    if(block_remainder >= (REQSIZE + OVERHEAD))
+    if (block_remainder >= (REQSIZE + OVERHEAD))
     {
         PUT(HDRP(alloc_ptr), PACK(size_needed, 1));
         PUT(FTRP(alloc_ptr), PACK(size_needed, 1));
@@ -275,24 +275,24 @@ void *mm_realloc(void *ptr, size_t size)
     //TODO: Check if it is possible to extend the current memory adress rather than just reserving more space
     void *newptr;
     size_t copySize;
-    
+
     newptr = mm_malloc(size);
-    
+
+    if (size == 0)
+    {
+        mm_free(ptr);
+        return NULL;
+    }
+
     if (newptr == NULL)
     {
         printf("ERROR: mm_realloc\n");
         exit(1);
     }
 
-    if(size == 0)
-    {
-        mm_free(ptr)
-        return NULL;
-    }
-
     copySize = GET_SIZE(HDRP(ptr));
     if (size < copySize)
-	copySize = size;
+        copySize = size;
     memcpy(newptr, ptr, copySize);
     mm_free(ptr);
     return newptr;
@@ -301,37 +301,37 @@ void *mm_realloc(void *ptr, size_t size)
 /*
  * coalecse -check the two neighboring blocks for alocation, if possible we will merge these blocks together
  */
-static void *coalesce(void *middle){
+static void *coalesce(void *middle) {
 
     size_t left = GET_ALLOC(FTRP(PREV_BLKP(middle)));
     size_t right = GET_ALLOC(HDRP(NEXT_BLKP(middle)));
     size_t size = GET_SIZE(HDRP(middle));
 
-    if(left && right)       //no free block to merge
+    if (left && right)      //no free block to merge
     {
-        return middle;      
+        return middle;
     }
-    else if(left && !right) //right neigbor is a free block
+    else if (left && !right) //right neigbor is a free block
     {
         size += GET_SIZE(HDRP(NEXT_BLKP(middle)));
         PUT(HDRP(middle), PACK(size, 0));
         PUT(FTRP(middle), PACK(size, 0));
     }
-    else if(!left && right) //left neigbor is a free block
+    else if (!left && right) //left neigbor is a free block
     {
         size += GET_SIZE(HDRP(PREV_BLKP(middle)));
         PUT(HDRP(PREV_BLKP(middle)), PACK(size, 0));
         PUT(FTRP(middle), PACK(size, 0));
         middle = PREV_BLKP(middle);
     }
-    else if(!left && !right)    //both neigbors are free blocks
+    else if (!left && !right)   //both neigbors are free blocks
     {
         size += GET_SIZE(HDRP(PREV_BLKP(middle))) + GET_SIZE(FTRP(NEXT_BLKP(middle)));
         PUT(HDRP(PREV_BLKP(middle)), PACK(size, 0));
         PUT(FTRP(NEXT_BLKP(middle)), PACK(size, 0));
         middle = PREV_BLKP(middle);
     }
-    
+
     return middle;
 }
 /*
